ebba29ae0d189558e84b73172c510880b7d277d1
“aquador” <Allison.quador@gmail.com>
2022-04-24 15:27:11 -0600

    update profile view with correct import

--- api/views/profile_views.py
+++ api/views/profile_views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
 
 
-from ..models.invite import Invite
+from ..models.profile import Profile
 from ..serializers import ProfileSerializer
 
 # Create your views here.
