60db92eae0045632ca7a2ed415752544b760c202
“aquador” <Allison.quador@gmail.com>
2022-04-29 20:03:31 -0600

    accepted invite route working

--- api/views/invite_views.py
+++ api/views/invite_views.py
@@ -92,24 +92,18 @@ class InviteDetail(generics.RetrieveUpdateDestroyAPIView):
         # If the data is not valid, return a response with the errors
         return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class InviteAccept(generics.RetrieveUpdateDestroyAPIView):
+class InviteAccept(generics.ListCreateAPIView):
     permission_classes=(IsAuthenticated,)
-    def partial_update(self, request, pk):
-        """Update Request"""
-        # Locate Invite
-        # get_object_or_404 returns a object representation of our Invite
-        invite = get_object_or_404(InviteModel, pk=pk)
-        # Check the invite's owner against the user making this request
-        if request.user != invite.host_id or request.user != invite.friend_id:
-            raise PermissionDenied('Unauthorized, you do not own this invite')
-
-        # Ensure the owner field is set to the current user's ID
-        request.data['invite']['host_id']['friend_id'] = request.user.id
-        # Validate updates with serializer
-        data = InviteSerializer(invite, data=request.data['invite'], partial=True)
-        if data.is_valid():
-            # Save & send a 204 no content
-            data.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-            # If the data is not valid, return a response with the errors
-        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = InviteSerializer
+    def get(self, request):
+        """Index request for all of the Accepted === True Invites"""
+        # Get all the invites:
+        # 1. query for all the invites --> here we use .all()
+        # Filter the invites by owner, so you can only see your owned invites
+        # the host is the owner of the invite
+        invites = InviteModel.objects.all()
+        # invites = InviteModel.objects.filter(host_id=request.user.id, friend_id=request.user.id)
+        # Run the data through the serializer
+        # 2. Serializer --> formats the data we just found
+        data = InviteSerializer(invites, many=True).data
+        return Response({ 'invites': data })
